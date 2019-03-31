7fbf60e07ff1be71603e0f2760e3c65612ef09b2
RKTP <archetype93@gmail.com>
2019-03-31 13:23:03 +0900

    [DEV] upload previous works on hacker news crawler

---
+++ hk_article.py
@@ -0,0 +1,36 @@
+import scrapy
+import json
+import logging
+import os, sys
+import pickle as pkl
+
+from os import path
+
+
+class HkaSpider(scrapy.Spider):
+    name = "hka"
+    data_dir = os.path.join('/home/rktp/test/hkcrawl/data') 
+
+    def start_requests(self):
+        metapaths = os.listdir(path.join(self.data_dir, 'meta'))
+        metafiles = []
+        for p in metapaths:
+            with open(os.path.join(self.data_dir, 'meta', p), 'rb') as f:
+                metafiles.append(pkl.load(f))
+
+        #urls = [ x['url'] for x in metafiles ]
+
+        #for url in urls:
+        for m in metafiles:
+            yield scrapy.Request(url=m['url'], callback=self.parse, meta={'id': m['id']})
+
+    def parse(self, response):
+        p_body = response.xpath('/html//p/text()').extract()
+        article_body = response.xpath('/html//article/text()').extract()
+
+        res = ''.join(p_body+article_body)
+        #logging.info(len(res))
+
+        if len(res) > 50 and not path.exists(path.join(self.data_dir,'article' ,str(response.meta.get('id'))+'.pkl')):
+            with open(os.path.join(self.data_dir, 'article', str(response.meta.get('id'))+'.pkl'), 'wb') as f:
+                pkl.dump(res, f)
---
+++ hk_base.py
@@ -0,0 +1,52 @@
+import scrapy
+import json
+import logging
+import os, sys
+import pickle as pkl
+
+from os import path
+#import http
+
+#from http import HTTPStatus
+
+
+class HkSpider(scrapy.Spider):
+    name = "hkc"
+    base_url = 'https://hacker-news.firebaseio.com/v0/{}.json?print=pretty'
+    data_dir = os.path.join('/home/rktp/test/hkcrawl/data') 
+
+    def start_requests(self):
+        urls = [
+            self.base_url.format('newstories'),
+            self.base_url.format('topstories'),
+            self.base_url.format('beststories')
+        ]
+
+        for url in urls:
+            #logging.info('Sending to:{}'.format(url))
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        id_list = json.loads(response.body)
+        #logging.info('Article Count: {}'.format(len(id_list)))
+        #logging.info(id_list)
+        for aid in id_list:
+            url = self.base_url.format('item/{}'.format(aid))
+            yield scrapy.Request(url=url, callback=self.parseArticle)
+
+    def parseArticle(self, response):
+        article_info = json.loads(response.body)
+        #logging.info(article_info)
+        #logging.info(type(article_info))
+        if article_info['type'] != 'story' or 'Ask HN' in article_info['title'] or 'Show HN' in article_info['title']:
+            pass #logging.info(article_info['title'])
+        elif 'url' not in list(article_info.keys()):
+            logging.info('URL not exist: '+str(article_info['id']))
+            return
+        elif path.exists(os.path.join(self.data_dir,'meta' ,str(article_info['id'])+'.pkl')):
+            logging.info('Duplicate article info: '+str(article_info['id']))
+            return
+        else:
+            with open(os.path.join(self.data_dir,'meta' ,str(article_info['id'])+'.pkl'),'wb') as f:
+                target = {'id': article_info['id'], 'score': article_info['score'], 'time': article_info['time'], 'title': article_info['title'], 'url': article_info['url']}
+                pkl.dump(target, f)
